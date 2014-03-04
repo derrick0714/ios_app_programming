47dcbce762b25e01ac9010b0ca1ae29f1dca38d0
derrick <derrick0714@gmail.com>
2014-03-04 00:49:09 -0500

    init notification implementation

---
+++ NotificationImplementation/NotificationImplementation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NotificationImplementation
+//
+//  Created by Xu Deng on 3/4/14.
+//  Copyright (c) 2014 Xu Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NotificationImplementation/NotificationImplementation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NotificationImplementation
+//
+//  Created by Xu Deng on 3/4/14.
+//  Copyright (c) 2014 Xu Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
